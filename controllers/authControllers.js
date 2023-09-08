d95205af537d677a22b8c0c7469eff7ac92fa592
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-09-08 11:14:19 +0100

    refactoring the code

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,107 +1,67 @@
-const User = require('../models/users');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const router = require('../routes/blogRoutes');
 
-const signup_get = (req, res) => res.render('signUp', { user: false });
-const signup_user = {
-    get: (req, res) => res.render('signUp', { user: true }),
-    post: (req, res) => res.send({ message: 'Not Yet !' })
+const User = require('../models/User');
+const SECRET_KEY = "HHHHHHHHHHH";
+
+async function login({ email, password }) {
+
+    if(!email) return { status: 400, body: 'Email is required' };
+    if(!password) return { status: 400, body: 'password is required' };
+
+    const user = await User.findOne({ email });
+    if(user === null) return { status: 404, body: 'Email or password is wrong' };
+
+    const isSamePassword = await bcrypt.compare(password, user.password);
+
+    if(isSamePassword === true){
+
+        const response = { id: user._id, email: user.email, username: user.name, role: user.role }
+        const token = jwt.sign(response, SECRET_KEY);
+
+        return { status: 200, body: { ...response, token } };
+
+    } else {
+        return { status: 404, body: 'Email or password is wrong' };
+    }
 }
 
-const login_get = (req, res) =>
-{
-    res.render('login');
-}
-const handleError = (err) =>
-{
-    console.log(err.message, err.code);
-    let errors = { name: '', email: '', password: '', test: '' };
+async function singup({ email, password, name }) {
 
-    // incorrect email
-    if (err.message === 'incorrect email')
-    {
-        errors.email = 'Wrong Email or password';
+    if(!email) return { status: 400, body: 'Email is required' };
+    if(!password) return { status: 400, body: 'password is required' };
+    if(!name) return { status: 400, body: 'name is required' };
+
+    const isUserExist = await User.findOne({ email: email });
+
+    if(isUserExist === null){
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = new User({ email, name, password: hashedPassword });
+        
+        try {
+
+            const savedUser = await user.save();
+
+            const response = {
+                id: savedUser._id,
+                email: savedUser.email,
+                username: savedUser.name,
+                role: user.role
+            }
+
+            const token = jwt.sign(response, SECRET_KEY);
+
+            return { status: 201, body: { ...response, token } };
+
+        } catch(err) {
+            throw new Error(err);
+        }
+
+    } else {
+        return { status: 404, body: 'User with this account already exist' };
     }
-
-    // incorrect password
-    if (err.message === 'incorrect password')
-    {
-        errors.password = 'Wrong Email or password';
-    }
-
-    // duplicate error code
-    if (err.code === 11000)
-    {
-        errors.email = 'Please enter a new email';
-        return errors;
-    }
-
-    // validation errors
-    if (err.message.includes('User validation failed:'))
-    {
-        Object.values(err.errors).forEach(({ properties }) =>
-        {
-            errors[properties.path] = properties.message;
-        });
-    }
-
-    return errors;
-};
-
-const maxAge = 3 * 24 * 60 * 60;
-
-const createToken = (id) =>
-{
-    return jwt.sign({ id }, 'net test secret', {
-        expiresIn: maxAge
-    });
-};
-
-const signup_post = async (req, res) =>
-{
-    const { name, email, password } = req.body;
-    try
-    {
-        const user = await User.create({ name, email, password });
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json({ user: user._id });
-    } catch (err)
-    {
-        const errors = handleError(err);
-        res.status(400).json({ errors });
-    }
-};
-
-const login_post = async (req, res) =>
-{
-    const { email, password } = req.body;
-
-    try
-    {
-        const user = await User.login(email, password);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json({ user: user._id });
-    } catch (err)
-    {
-        const errors = handleError(err);
-        res.status(400).json({ errors });
-    }
-};
-
-
-const logout_get = (req, res) =>
-{
-    res.cookie('jwt', '', { maxAge: 1 });
-    res.redirect('/login');
 }
 
-module.exports = {
-    login_get,
-    signup_get,
-    login_post,
-    signup_post,
-    logout_get,
-    signup_user
-}
+module.exports = { singup, login }
