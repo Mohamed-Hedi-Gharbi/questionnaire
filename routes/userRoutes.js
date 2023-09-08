051fc764a27255911c4a820ed5750e3e8e694ad7
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-09-08 11:15:46 +0100

    add new route for users

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.post('/start', (req, res) => {});
+
+router.post('/submit', (req, res) => {});
+
+router.get('/results', (req, res) => {});
+
+module.exports = router;
