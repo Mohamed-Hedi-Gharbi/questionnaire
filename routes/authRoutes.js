fdc6223fc46419919bee2fe7fea8d89802022559
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-09-08 11:15:07 +0100

    remove unecessary endpoints and we change it to become restful api instead of returning html

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,46 @@
 const { Router } = require('express');
-const authController = require('../controllers/authControllers');
-
+const { login, singup } = require('../controllers/authControllers');
 
 const router = Router();
 
+router.post('/login', async(req, res) => {
+    try {
 
-router.get('/', (req, res) => res.redirect('/login'));
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
-router.get('/signup/user', authController.signup_user.get);
-router.post('/signup/user', authController.signup_user.post);
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
-router.get('/logout', authController.logout_get);
+        const { email, password } = req.body;
+        const respond = await login({ email, password });
 
+        if(respond.body.token){
+            res.cookie('token', respond.body.token);
+        }
+
+        res.status(respond.status).send(respond.body);
+        
+
+    } catch (err) {
+        res.status(500).send({ message: 'Internal Server Error', error: err.message });
+    }
+});
+
+router.post('/signup', async(req, res) => {
+    try {
+        
+        const { email, password, name } = req.body;
+        const respond = await singup({ email, password, name });
+
+        if(respond.body.token){
+            res.cookie('token', respond.body.token);
+        }
+        
+        res.status(respond.status).send(respond.body);
+
+    } catch (err) {
+        res.status(500).send({ message: 'Internal Server Error', error: err.message });
+    }
+});
+
+router.get('/logout', async(req, res) => {
+    res.cookie('token', '', { expires: new Date(0) });
+    res.status(204).send();
+});
 
 module.exports = router;
