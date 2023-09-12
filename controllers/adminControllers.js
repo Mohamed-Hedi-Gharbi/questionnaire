cf7a454a0982e07515f1f02976dab0c96f043227
gharbi-collab <mohamedhedigharbi101@gmail.com>
2023-09-12 16:28:28 +0100

    writing some crud

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,58 +1,130 @@
 const Quizz = require("../models/Quizz");
 const Specific_Quizz = require("../models/Specific_Quizz");
 
-async function getAllQuizz(language, owner_id, supportedLanguage = ['javascript', 'cs', 'php']) {
+async function _getQuizz(DB, options, supportedLanguage = ['javascript', 'cs', 'php']) {
   
-  const index = supportedLanguage.findIndex(e => e === language);
-
-  if(index === -1) return { status: 400, body: 'Invalid language' };
-
   try {
-
-    const quizzes = await Quizz.find();
-    const specific_quizz = await Specific_Quizz.find({ owner_id });
-
-    for(let { quizz } of specific_quizz) {
-      quizzes.push(quizz)
-    }
-
-    return { status: 200, body: quizzes };
+    
+    return await DB.find(options);
 
   } catch (error) {
     throw new Error(error);
   }
+
 }
 
-async function addQuizz({ language, quizz, answers, correct_answer, owner_id }) {
+async function getSpecificQuizz(language, owner_id, supportedLanguage = "") {
+  try {
+  
+    const x = await _getQuizz(Specific_Quizz, { owner_id });
+    const listOfQuiz = [];
 
+    for(let index in x){
+      const { quizz } = x[index];
+      if(language === quizz.language) {
+      listOfQuiz.push(quizz);
+      }      
+    }
+
+    return listOfQuiz;
+  
+  } catch (error) {
+    throw new Error(error);
+  }
+
+}
+
+async function getInitialQuizz(language) {
+  try {
+  
+    const options = { language };
+    return _getQuizz(Quizz, options);
+  
+  } catch (error) {
+    throw new Error(error);
+  }
+
+}
+
+async function addQuizz({ language, question, answers, correct_answer, owner_id }) {
   try{
+  
+    const _quizz = new Quizz({ language, question, answers, correct_answer });
 
-    if(!language) return { status: 400, body: 'Language is required' };
-    if(!quizz) return { status: 400, body: 'Please provide the quizz question' };
-    if(!correct_answer) return { status: 400, body: 'Please provide correct answer' };
-    if(!answers || answers.length === 0) return { status: 400, body: 'Invalid answers' };
+    try{
+      const new_quizz = new Specific_Quizz({ quizz: _quizz, owner_id });
+
+      try {
+        const saveQuizz = await new_quizz.save();
+
+        return {
+          status: 201,
+          body: {
+            error: false,
+            message: 'Quizz added successfully',
+            id: saveQuizz._id
+          }
+        }
+      } catch (error) {
+        throw Error(error);
+      }
+      
+    } catch(error) {
+      throw Error(error);
+    }
   
-    const _quizz = new Quizz({ language, quizz, answers, correct_answer });
-    const new_quizz = new Specific_Quizz({ quizz: _quizz, owner_id });
-  
-    const saveQuizz = await new_quizz.save();
+
+  } catch (error) {
+    console.log(error.message);
+    throw Error(error);
+  }
+
+}
+
+async function updateQuizz({ language, question, answers, correct_answer, question_id, owner_id }) {
+
+  try {
+    const quizz = { language, answers, question, correct_answer, explanation: "" };
+
+    await Specific_Quizz.findById(question_id)
+      .updateOne({ owner_id }, { quizz });
 
     return {
-      status: 201,
+      status: 200,
       body: {
         error: false,
-        message: 'Quizz added successfully',
-        id: saveQuizz._id
+        message: 'Quizz updated successfully',  
       }
     }
 
   } catch (error) {
+    console.log(error.message);
     throw new Error(error);
   }
+}
 
+async function deleteQuizz(owner_id, question_id) {
+  try {
+    await Specific_Quizz.findById(question_id)
+      .deleteOne({ owner_id });
+    
+    return {
+      status: 200,
+      body: {
+        error: false,
+        message: 'Quizz deleted successfully',  
+      }
+    }
+
+  } catch (error) {
+    throw Error(error);
+  }
 }
 
 module.exports = {
-  getAllQuizz,
-  addQuizz
+  getInitialQuizz,
+  getSpecificQuizz,
+  addQuizz,
+  updateQuizz,
+  deleteQuizz
 }
